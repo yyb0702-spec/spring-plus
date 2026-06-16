f2f1ab407bdb101079e1186aa7d1a092a00d3f2a
깡단 <yyb19920702@gmail.com>
2026-06-16 15:26:18 +0900

    fix: 1-2 코드개선 User 생성자에 nickname도 들어갈수있도록 수정, jwt토큰에 nickname 추가

--- src/main/java/org/example/expert/domain/user/entity/User.java
+++ src/main/java/org/example/expert/domain/user/entity/User.java
@@ -18,23 +18,28 @@ public class User extends Timestamped {
     @Column(unique = true)
     private String email;
     private String password;
+    // 닉네임 콜롬추가 중복값은 기본설정으로 허용,null 불가
+    @Column(nullable = false)
+    private String nickname;
     @Enumerated(EnumType.STRING)
     private UserRole userRole;
 
-    public User(String email, String password, UserRole userRole) {
+    public User(String email, String password, UserRole userRole,String nickname) {
         this.email = email;
         this.password = password;
         this.userRole = userRole;
+        this.nickname = nickname;
     }
 
-    private User(Long id, String email, UserRole userRole) {
+    private User(Long id, String email, UserRole userRole,String nickname) {
         this.id = id;
         this.email = email;
         this.userRole = userRole;
+        this.nickname = nickname;
     }
 
     public static User fromAuthUser(AuthUser authUser) {
-        return new User(authUser.getId(), authUser.getEmail(), authUser.getUserRole());
+        return new User(authUser.getId(), authUser.getEmail(), authUser.getUserRole(),authUser.getNickname());
     }
 
     public void changePassword(String password) {
