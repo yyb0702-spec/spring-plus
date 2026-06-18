b6f0093761c3a64e226427f1102411bdcd0f7b17
깡단 <yyb19920702@gmail.com>
2026-06-18 19:20:11 +0900

    fix: 주석제거

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -11,10 +11,7 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long>,TodoCustromRepository {
-/*
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
-    Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
-*/
+
     @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u " +
             "WHERE (:weather IS NULL OR t.weather = :weather) " +
             "AND (:startDate IS NULL OR t.modifiedAt >= :startDate) " +
@@ -25,10 +22,4 @@ public interface TodoRepository extends JpaRepository<Todo, Long>,TodoCustromRep
                       @Param("endDate") LocalDateTime endDate,
                       Pageable pageable);
 
-    /*
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
-            "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
-    */
 }
